cbdcdbe249de678c4d7bb118576648537327f977
theZoid9 <zaidakhalwaya6@gmail.com>
2026-08-14 13:06:55 +0200

    Rebuild Supabase motion controller

--- public/config.js
+++ public/config.js
@@ -2,7 +2,8 @@ const SUPABASE_URL = "https://qldofrojnpwzwhpqurbl.supabase.co";
 
 const SUPABASE_KEY = "s7PS6DHq0Kpx16Mb";
 
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+window.supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
